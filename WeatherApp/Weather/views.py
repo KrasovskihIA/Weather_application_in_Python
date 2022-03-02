7c9685030d5c44620d3178f5d3afbebe0c9cc424
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-03-02 20:50:47 +0400

    Add a city
    Настройка форма, добавления города

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = 'd4e54608dafc5d0ba494b7a62832b70e'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
 
-    city = 'London'
-    res = requests.get(url.format(city)).json()
+    if (request.method =='POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
     
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"]
-    }
-    context = {'info': city_info}
+
+    cities = City.objects.all()
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form':form}
     return render(request, 'Weather/index.html', context)
