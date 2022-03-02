c44d3a939f4c7e0e71487f00c8d3d835f46d1f78
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-03-02 19:14:41 +0400

    API openweather

--- WeatherApp/Weather/views.py
+++ WeatherApp/Weather/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'Weather/index.html')
+    appid = 'd4e54608dafc5d0ba494b7a62832b70e'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+    
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+    context = {'info': city_info}
+    return render(request, 'Weather/index.html', context)
